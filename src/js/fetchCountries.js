73bcc362b3abe276841d36426cdbd083d950e8c6
SerheiYakimov <ysergey2901@gmail.com>
2021-09-10 01:00:57 +0300

    correction fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -10,4 +10,16 @@
 
 export default fetchCountries;
 
+
+// const BASE_URL = 'https://restcountries.eu';
+
+// function fetchCountries(searchQuery) {
+//     const name = searchQuery;    
+//     return fetch(`${BASE_URL}/rest/v2/name/${name}`)
+//     .then(response => response.json());
+   
+// }
+
+// export default fetchCountries;
+
         
